09020223cb7570bb30c82a06b345ce0f8d81d843
PaulaIbagere <paulaibagere@gmail.com>
2023-03-17 15:48:22 -0700

    without e an q

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/*
+ * main - main function
+ * Return: alwayse 0
+ * for - for loop
+ */
+int main(void)
+{
+	char a;
+
+	for (a = 'a'; a <= 'z'; a++)
+	{
+		if (a != 'q' && a != 'e')
+			putchar(a);
+	}
+	putchar('\n');
+	return (0);
+}
