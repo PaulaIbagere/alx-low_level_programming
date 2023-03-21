249ba8225d92bec1c6f2865e2c8f3b8e626545ac
PaulaIbagere <paulaibagere@gmail.com>
2023-03-21 11:49:41 -0700

    Alphabet from a to z

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * print_alphabet - to print alphabets from a to z
+ * Return: alwayse 0
+ */
+void print_alphabet(void)
+{
+	int i;
+
+	for (i = 'a'; i <= 'z'; i++)
+		_putchar(i);
+	_putchar('\n');
+}
