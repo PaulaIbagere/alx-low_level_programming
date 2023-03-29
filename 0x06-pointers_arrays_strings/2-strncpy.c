df994e33dc01ae1bf85fa081f0ba1d3610e94a13
PaulaIbagere <paulaibagere@gmail.com>
2023-03-29 10:05:32 -0700

    copy

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ * *_strncpy - function to copy string
+ * @dest: destination
+ * @src: sorce
+ * @n: number of strings
+ * Return: dest
+ */
+char *_strncpy(char *dest, char *src, int n)
+{
+	int j;
+
+	j = 0;
+	while (j < n && src[j] != '\0')
+	{
+		dest[j] = src[j];
+		j++;
+	}
+	while (j < n)
+	{
+		dest[j] = '\0';
+		j++;
+	}
+	return (dest);
+}
