99e67b84f8c5333940ed69706b175fa8f75eb797
PaulaIbagere <paulaibagere@gmail.com>
2023-03-29 08:24:00 -0700

    string concatination

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,12 @@
+#include "main.h"
+#include <string.h>
+/**
+ * *_strcat - function to concatinate strings
+ * @dest: first parameter
+ * @src: second parameter
+ * Return: i
+ */
+char *_strcat(char *dest, char *src)
+{
+	return (strcat(dest,src));
+}
