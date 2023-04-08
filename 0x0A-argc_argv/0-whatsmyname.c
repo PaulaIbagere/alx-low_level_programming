2871306276705d7e987dd2a784d10859b64f4fe2
PaulaIbagere <paulaibagere@gmail.com>
2023-04-08 08:34:31 -0700

    name of the file using argc and argv

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - main function
+ * @argc: first parameter
+ * @argv: second parameter
+ * Return: 0
+ */
+int main(int argc, char *argv[])
+{
+	printf("%s\n", argv[argc - 1]);
+	return (0);
+}
