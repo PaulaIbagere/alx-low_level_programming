6717d7759189244c0059cd87409dcf26cd4c9a00
PaulaIbagere <paulaibagere@gmail.com>
2023-04-08 09:35:56 -0700

    multiplication of parameters

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+/**
+ * main - main function
+ * @argc: first parameter
+ * @argv: second parameter
+ * Return: 1
+ */
+int main(int argc, char **argv)
+{
+	int i;
+	int result = 1;
+
+	if (argc < 4)
+	{
+		for (i = 1; i < argc; i++)
+		{
+			int x = strtol(argv[i], NULL, 10);
+			result = result * x;
+		}
+		printf("%d\n", result);
+	}
+	else
+	{
+		printf("Error\n");
+	}
+	return (1);
+}
