43427e3c91486536aebd50239cfdd923dd454eba
PaulaIbagere <paulaibagere@gmail.com>
2023-04-10 08:32:16 -0700

    some changes to the 3-puts file

--- 0x09-static_libraries/3-puts.c
+++ 0x09-static_libraries/3-puts.c
@@ -1,15 +1,11 @@
 #include "main.h"
-#include<stdio.h>
+#include <stdio.h>
 /**
- * _puts - to put char
- * @str: first parameter
- * main - entry point
- * Return: Always 0 (Success)
+ * _puts - function that prints a string, followed by a new line, to stdout
+ * @str: parameter
+ * Return: puts
  */
-void _puts(char *str);
-int main(void)
+void _puts(char *str)
 {
-	puts("\"Programming is like building a multilingual puzzle");
-	return (0);
+	 puts(str);
 }
-
