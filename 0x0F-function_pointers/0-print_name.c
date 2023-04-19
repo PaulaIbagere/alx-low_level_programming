663dd9e3cbb0229fdda0adb0504614df03022084
PaulaIbagere <paulaibagere@gmail.com>
2023-04-19 09:05:22 -0700

    Printing out name in uppercase and lowercase

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,13 @@
+#include "function_pointers.h"
+/**
+ * print_name - function to print name using function pointer
+ * @name: string to add
+ * @f: pointer to the function
+ * Return: Nothing
+ */
+void print_name(char *name, void (*f)(char *))
+{
+	if (name == NULL || f == NULL)
+		return;
+	(*f)(name);
+}
